d5521f45b53ae21f5e81ab0c1b77808c964c519f
蔡承諺 <ym851010@gmail.com>
2021-02-13 15:33:32 +0800

    fix: amend code

--- download_images.py
+++ download_images.py
@@ -18,22 +18,49 @@ def main():
     mongo_collection = client['movies']['info']
     data = mongo_collection.find({}, {
         "id": 1,
+        "movie_id": 1,
         "image_path": 1,
         "poster_path": 1
     })
     data_img = []
     for d in data:
-        for image in d['image_path']:
-            image_img = requests.get(image, timeout=10).content
-            with open(
-                    './../api/images/' + str(d['id']) + '/' + str(d['id']) +
-                    '.jpg', 'wb') as f:
-                f.write(backdrop_img)
-        poster_img = requests.get(d["poster_path"], timeout=10).content
-        with open('./../api/images/poster_path_' + str(d['id']) + '.jpg',
-                  'wb') as f:
-            f.write(poster_img)
-        break
+        try:
+            for i, image in enumerate(d['image_path']):
+                file_image = './../api/public/images/backdrop/image_path_' + str(
+                    d['movie_id']) + '/' + str(
+                        d['movie_id']) + '_' + str(i) + '.jpg'
+                if not os.path.exists(file_image):
+                    image_img = requests.get(image, timeout=10).content
+                    createFolder('./../api/public/images/backdrop/image_path_' +
+                                 str(d['movie_id']) + '/')
+                    with open(file_image, 'wb') as f:
+                        f.write(image_img)
+                    print('back' + str(d['movie_id']) + '_' + str(i) + ' OK')
+                else:
+                    print('back' + str(d['movie_id']) + '_' + str(i) +
+                          ' exists')
+                    continue
+        except TypeError:
+            print('back' + str(d['movie_id']) + ' None')
+            pass
+        try:
+            file_poster = './../api/public/images/poster/poster_path_' + str(
+                d['movie_id']) + '.jpg'
+            if not os.path.exists(file_poster):
+                poster_img = requests.get(d["poster_path"], timeout=10).content
+                with open(file_poster, 'wb') as f:
+                    f.write(poster_img)
+                print('poster' + str(d['movie_id']) + ' OK')
+            else:
+                print('poster' + str(d['movie_id']) + ' exists')
+        except TypeError:
+            print('poster' + str(d['movie_id']) + ' None')
+            pass
 
 
-main()
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print('Error: Creating directory. ' + directory)
